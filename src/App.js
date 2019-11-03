f6a30fd307af7deda775e9f4afd6ae0518454f7d
Karina Ionkina <karina.ionkina15@gmail.com>
2019-11-03 01:59:01 -0500

    search

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
 import React from 'react';
 import axios from 'axios';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+let test_user_password_access_token = process.env.TEST_USER_PASSWORD_ACCESS_TOKEN
+
+
+const divStyle = {
+    width: '500px',
+    height: "50px",
+    fontSize: "30px",
+};
+
+const buttonStyle = {
+    
+    width: "100px",
+    height: "55px",
+    marginTop: "-10px",
+    marginBottom: "8px"
+}
+
+    const hashtag = {
+	fontSize: "40px",
+	display: "inline-block",
+    marginTop: "20px",
+    }
 
 class App extends React.Component {
   constructor(props) {
@@ -28,11 +54,14 @@ class App extends React.Component {
   render(){
   return (
     <div className="App">
-          <label>
-          Hashtag:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-      <button onClick={this.handleSubmit}></button>
+    <center>
+    <span style={hashtag}> # </span> <TextField style={divStyle}
+          id="outlined-basic"
+          margin="normal"
+          variant="outlined"
+    value={this.state.value} onChange={this.handleChange}
+    /> <Button variant = "outlined" style={buttonStyle} onClick={this.handleSubmit}>Search </Button>
+    </center>
     </div>
   );
   };
